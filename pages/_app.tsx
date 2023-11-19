79c64a2c8ddc5f5da5fedd5b32050ae022f799d0
Blanca Serrano Marfil <blanca@Blancas-MacBook-Air.local>
2023-11-19 12:59:03 +0100

    moving Head to document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,14 @@ import { ThemeProvider } from "styled-components";
 import defaultTheme from "../styles/theme";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Authors manager</title>
-        <link rel="stylesheet" href="/fonts/Vollkorn-VaraibleFont_wght.ttf" />
-        <link rel="stylesheet" href="/fonts/Vollkorn-Italic-VariableFont_wght.ttf" />
-      </Head>
-      <ThemeProvider theme={defaultTheme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
